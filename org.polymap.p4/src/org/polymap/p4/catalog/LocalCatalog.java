68795febe0b4d209927db4cfb77673a1b30296b9
Falko Bräutigam <falko@polymap.de>
2018-11-28 16:28:43 +0100

    #101: Start Project (http://github.com/Mapzone/mapzone/issues/101)
      - import shapefile into local datastore
      - create layer with sample style and image cache
      - keep WMS but do not add layer

--- org.polymap.p4/src/org/polymap/p4/catalog/LocalCatalog.java
+++ org.polymap.p4/src/org/polymap/p4/catalog/LocalCatalog.java
@@ -1,6 +1,6 @@
 /* 
  * polymap.org
- * Copyright (C) 2015-2016, Falko Bräutigam. All rights reserved.
+ * Copyright (C) 2015-2018, Falko Bräutigam. All rights reserved.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -16,21 +16,38 @@ package org.polymap.p4.catalog;
 
 import static org.polymap.core.CorePlugin.getDataLocation;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.Serializable;
+import java.net.URL;
+import java.nio.file.Files;
 
 import org.geotools.data.DataAccess;
+import org.geotools.data.FeatureStore;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.store.ContentFeatureSource;
+import org.opengis.feature.simple.SimpleFeatureType;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
-
 import org.polymap.core.catalog.IMetadata;
 import org.polymap.core.catalog.local.LocalMetadataCatalog;
 import org.polymap.core.catalog.resolve.IResolvableInfo;
+import org.polymap.core.data.rs.RDataStore;
 import org.polymap.core.data.rs.catalog.RServiceResolver;
+import org.polymap.core.data.rs.lucene.LuceneQueryDialect;
 import org.polymap.core.data.wms.catalog.WmsServiceResolver;
 
 import org.polymap.rhei.fulltext.store.lucene.LuceneFulltextIndex;
@@ -47,12 +64,11 @@ import org.polymap.recordstore.lucene.LuceneRecordStore;
 public class LocalCatalog
         extends LocalMetadataCatalog {
 
-    private static Log log = LogFactory.getLog( LocalCatalog.class );
-
     public static final String      LOCAL_FEATURES_STORE_ID = "_local_features_store_";
 
     public static final String      WORLD_BACKGROUND_ID = "_world_background_";
-
+    public static final String      WORLD_BORDER_RES = "TM_WORLD_BORDERS-0.3";
+    
     private LuceneRecordStore       store;
 
     private LuceneFulltextIndex     index;
@@ -65,7 +81,7 @@ public class LocalCatalog
         
         init( new RecordStoreAdapter( store ), index );
         
-        createEntries();
+        checkInitContents();
     }
 
     
@@ -89,7 +105,7 @@ public class LocalCatalog
             return entry( LOCAL_FEATURES_STORE_ID, new NullProgressMonitor() ).get();
         }
         catch (Exception e) {
-            throw new RuntimeException( e );
+            throw Throwables.propagate( e );
         }
     }
     
@@ -103,7 +119,7 @@ public class LocalCatalog
             return P4Plugin.allResolver().resolve( metadata ).get();
         }
         catch (Exception e) {
-            throw new RuntimeException( e );
+            throw Throwables.propagate( e );
         }
     }
     
@@ -117,12 +133,15 @@ public class LocalCatalog
             return info.getServiceInfo().createService( new NullProgressMonitor() );
         }
         catch (Exception e) {
-            throw new RuntimeException( e );
+            throw Throwables.propagate( e );
         }
     }
     
-    
-    protected void createEntries() throws Exception {
+
+    /**
+     * Creates standard entries.
+     */
+    protected void checkInitContents() throws Exception {
         // check empty
         if (query( ALL_QUERY, new NullProgressMonitor() ).execute().size() == 0) {
             // create standard entries
@@ -137,7 +156,7 @@ public class LocalCatalog
                 });
                 update.newEntry( metadata -> {
                     metadata.setIdentifier( WORLD_BACKGROUND_ID );
-                    metadata.setTitle( "World" );
+                    metadata.setTitle( "Example WMS" );
                     metadata.setDescription( "Default background and demo layers from mapzone.io." );
                     metadata.setType( "Service" );
                     metadata.setFormats( Sets.newHashSet( "WMS", "WFS" ) );
@@ -150,6 +169,42 @@ public class LocalCatalog
 //                });
                 update.commit();
             }
+            
+            // copy resources/TM_WORLD_BORDERS-0.3.zip
+            URL url = P4Plugin.instance().getBundle().getEntry( "resources/TM_WORLD_BORDERS-0.3.zip" );
+            File temp = Files.createTempDirectory( "p4-catalog-init-" ).toFile();
+            File shp = null;
+            try (
+                ZipInputStream zip = new ZipInputStream( url.openStream() );
+            ){
+                for (ZipEntry entry=zip.getNextEntry(); entry!=null; entry=zip.getNextEntry()) {
+                    File f = new File( temp, FilenameUtils.getName( entry.getName() ) );
+                    try (FileOutputStream out = new FileOutputStream( f )) {
+                        IOUtils.copy( zip, out );
+                    }
+                    if (f.getName().endsWith( ".shp" )) {
+                        shp = f;
+                    }
+                }
+            }
+            
+            Map<String,Serializable> params = new HashMap<String,Serializable>();
+            params.put( ShapefileDataStoreFactory.URLP.key, shp.toURI().toURL() );
+            params.put( ShapefileDataStoreFactory.CREATE_SPATIAL_INDEX.key, Boolean.FALSE );
+
+            ShapefileDataStoreFactory dsf = new ShapefileDataStoreFactory();
+            ShapefileDataStore shapeDs = (ShapefileDataStore)dsf.createNewDataStore( params );
+            ContentFeatureSource shapeFs = shapeDs.getFeatureSource();
+            SimpleFeatureType schema = shapeFs.getSchema();
+
+            LuceneRecordStore rs = new LuceneRecordStore( P4Plugin.featureStoreDir(), false );
+            RDataStore ds = new RDataStore( rs, new LuceneQueryDialect() );            
+            ds.createSchema( schema );
+            FeatureStore fs = (FeatureStore)ds.getFeatureSource( schema.getName() );
+            fs.addFeatures( shapeFs.getFeatures() );
+            
+            ds.dispose();            
+            FileUtils.deleteDirectory( temp );
         }
     }
 
--- org.polymap.p4/src/org/polymap/p4/project/ProjectRepository.java
+++ org.polymap.p4/src/org/polymap/p4/project/ProjectRepository.java
@@ -1,6 +1,6 @@
 /* 
  * polymap.org
- * Copyright (C) 2015-2016, Falko Bräutigam. All rights reserved.
+ * Copyright (C) 2015-2018, Falko Bräutigam. All rights reserved.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -14,10 +14,13 @@
  */
 package org.polymap.p4.project;
 
+import static org.polymap.core.data.DataPlugin.ff;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.filter.expression.PropertyName;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import org.apache.commons.logging.Log;
@@ -29,12 +32,21 @@ import org.polymap.core.CorePlugin;
 import org.polymap.core.catalog.IMetadata;
 import org.polymap.core.catalog.resolve.IResourceInfo;
 import org.polymap.core.catalog.resolve.IServiceInfo;
+import org.polymap.core.data.image.cache304.ImageCacheProcessor;
+import org.polymap.core.data.pipeline.ProcessorExtension;
 import org.polymap.core.data.util.Geometries;
 import org.polymap.core.project.EnvelopeComposite;
 import org.polymap.core.project.ILayer;
+import org.polymap.core.project.ILayer.ProcessorConfig;
 import org.polymap.core.project.IMap;
 import org.polymap.core.runtime.session.SessionContext;
 import org.polymap.core.runtime.session.SessionSingleton;
+import org.polymap.core.style.DefaultStyle;
+import org.polymap.core.style.model.FeatureStyle;
+import org.polymap.core.style.model.feature.ConstantColor;
+import org.polymap.core.style.model.feature.ConstantNumber;
+import org.polymap.core.style.model.feature.FilterMappedPrimitives;
+import org.polymap.core.style.model.feature.PolygonStyle;
 
 import org.polymap.model2.runtime.EntityRepository;
 import org.polymap.model2.runtime.UnitOfWork;
@@ -58,6 +70,7 @@ public class ProjectRepository {
 
     private static EntityRepository     repo;
 
+    
     static {
         try {
             File dir = new File( CorePlugin.getDataLocation( P4Plugin.instance() ), "project" );
@@ -86,6 +99,7 @@ public class ProjectRepository {
         }
     }
     
+    
     protected static void checkInitRepo() {
         try (
             UnitOfWork uow = repo.newUnitOfWork()
@@ -107,19 +121,62 @@ public class ProjectRepository {
                 
                 // default background layer from mapzone.io
                 try {
+                    // WMS
                     NullProgressMonitor monitor = new NullProgressMonitor();
-                    IMetadata md = P4Plugin.localCatalog().entry( LocalCatalog.WORLD_BACKGROUND_ID, monitor ).get();
+//                    IMetadata md = P4Plugin.localCatalog().entry( LocalCatalog.WORLD_BACKGROUND_ID, monitor ).get();
+//                    IServiceInfo service = (IServiceInfo)AllResolver.instance().resolve( md ).get();
+//                    for (IResourceInfo res : service.getResources( monitor )) {
+//                        if ("Simple".equalsIgnoreCase( res.getName() ) ) {
+//                            ILayer layer = uow.createEntity( ILayer.class, null, (ILayer proto) -> {
+//                                proto.label.set( "World - WMS" );
+//                                proto.description.set( res.getDescription().orElse( null ) );
+//                                proto.resourceIdentifier.set( AllResolver.resourceIdentifier( res ) );
+//                                proto.orderKey.set( 1 );
+//                                return proto;
+//                            });
+//                            layer.parentMap.set( map );
+//                            break;
+//                        }
+//                    }
+                    // Features
+                    IMetadata md = P4Plugin.localCatalog().entry( LocalCatalog.LOCAL_FEATURES_STORE_ID, monitor ).get();
                     IServiceInfo service = (IServiceInfo)AllResolver.instance().resolve( md ).get();
                     for (IResourceInfo res : service.getResources( monitor )) {
-                        if ("Simple".equalsIgnoreCase( res.getName() ) ) {
+                        if (res.getName().equals( LocalCatalog.WORLD_BORDER_RES )) {
                             ILayer layer = uow.createEntity( ILayer.class, null, (ILayer proto) -> {
-                                proto.label.set( "World" );
-                                proto.description.set( res.getDescription().orElse( null ) );
+                                proto.label.set( "World Borders" );
+                                proto.description.set( "Sample features imported from thematicmapping.org/ downloads/world_borders.php.\n\n"
+                                        + "You may use this layer to try out styling of features." );
                                 proto.resourceIdentifier.set( AllResolver.resourceIdentifier( res ) );
-                                proto.orderKey.set( 1 );
+                                proto.orderKey.set( 2 );
+                                // style
+                                FeatureStyle style = P4Plugin.styleRepo().newFeatureStyle();
+                                PolygonStyle polygon = DefaultStyle.fillPolygonStyle( style );
+                                polygon.fill.get().color.createValue( ConstantColor.defaults( 0xF5, 0xE4, 0x9C ) );
+                                polygon.stroke.get().color.createValue( ConstantColor.defaults( 0xD4, 0xAB, 0x89 ) );
+                                polygon.stroke.get().width.createValue( ConstantNumber.defaults( 0.2 ) );
+                                // opacity: see NumberGradient2FilterEditor
+                                int upper = 1300000;
+                                int breakpoints = 9;
+                                PropertyName prop = ff.property( "AREA" );
+                                FilterMappedPrimitives<Double> mapped = polygon.fill.get().opacity.createValue( FilterMappedPrimitives.defaults() );
+                                mapped.add( ff.less( prop, ff.literal( upper/breakpoints ) ), 0.1 );
+                                for (int i=0; i<breakpoints; i++) {
+                                    mapped.add( ff.and( 
+                                            ff.greaterOrEqual( prop, ff.literal( upper/breakpoints*i ) ), 
+                                            ff.less( prop, ff.literal( upper/breakpoints*(i+1) ) ) ), 0.1 + (1d/breakpoints*i) );
+                                }
+                                mapped.add( ff.greaterOrEqual( prop, ff.literal( upper ) ), 1d );
+                                style.store();
+                                proto.styleIdentifier.set( style.id() );
+                                // image cache
+                                ProcessorExtension ext = ProcessorExtension.forType( ImageCacheProcessor.class.getName() )
+                                        .orElseThrow( () -> new RuntimeException( "ImageCacheProcessor is not available. Is the plugin installed?" ) );
+                                proto.processorConfigs.createElement( ProcessorConfig.init( ext ) );
                                 return proto;
                             });
                             layer.parentMap.set( map );
+                            break;
                         }
                     }
                 }
